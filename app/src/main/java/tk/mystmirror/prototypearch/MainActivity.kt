bec01ab52cb180b9bb1e77eee20f92eed1053b9d
MystMirroR <mystmirror@mail.ru>
2021-07-31 19:46:14 +0300

    Initial commit

---
+++ app/src/main/java/tk/mystmirror/prototypearch/MainActivity.kt
@@ -0,0 +1,18 @@
+package tk.mystmirror.prototypearch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import tk.mystmirror.prototypearch.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/tk/mystmirror/prototypearch/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package tk.mystmirror.prototypearch.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
